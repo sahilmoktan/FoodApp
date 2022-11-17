0fc5e9aaa7c4f248df6e8366a2e0b333a6b58640
Sahil <moktanshail9@gmail.com>
2022-11-17 08:06:29 +0545

    initial work of UI work

---
+++ Client/src/store.js
@@ -0,0 +1,15 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from "redux";
+import logger from "redux-logger";
+// import addToCartSlice from "./components/addToCart/addToCart.slice";
+const reducer = combineReducers({
+  // register: Register,
+  // count: addToCartSlice,
+});
+
+const store = configureStore({
+  reducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+});
+
+export default store;
